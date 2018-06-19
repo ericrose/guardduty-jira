91782b3aaa40b9d236a92c5f85434726e8f3c060
errose <errose@amazon.com>
2018-06-19 02:09:19 +0000

    more cleanup

--- src/main.py
+++ src/main.py
@@ -8,10 +8,8 @@ def lambda_handler(event, context):
 def main(event, is_lambda=True):
     message = event['detail']
     try:
-        #if PagerDuty.is_triggered_alert(message_data) and not Ticket.exists(message_data):  
         ticket = Ticket(message, is_lambda)
         ticket.create()
-         # TODO update_pager_duty_with_ticket_info()
     except Exception as e:
         print('Error: {0}'.format(e))
 
@@ -19,5 +17,4 @@ def main(event, is_lambda=True):
 if __name__ == '__main__':
     with open('../tests/sample.json') as f:
         event = json.load(f)
-    #pprint(event)    
     main(event, is_lambda=False)
